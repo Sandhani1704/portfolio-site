33e429d81fd8b15dfd8e41e36bcecc5dee8765ca
Anna Galkina <sandhani1704@yandex.ru>
2020-11-12 03:15:41 +0300

    добавлены новые блоки в css

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,27 @@
 import React from 'react';
 import ProfilePic from './images/photo_pic.jpg'
-import facebook from './images/facebook_color_white.svg'
-import VK from './images/vk_color_white.svg'
+import facebook from './images/facebook_icon.png'
+import telegram from './images/telegram_icon.png'
 import github from './images/icon-github.png'
+import ProjectsImage from './images/banner1.png'
 
 function App() {
     return (
         <>
             <header className="header">
-                <p>Portfolio</p>
+                <p className="header__title">Portfolio</p>
                 <nav className="navbar">
                     <ul className="navbar__nav">
                         <li className="navbar__item">
-                            <a className="navbar__link" href="#" target="_blank"><img class="navbar__social-icon" src={facebook}
+                            <a className="navbar__link" href="#" target="_blank"><img className="navbar__social-icon" src={facebook}
                                 alt="Facebook" /></a>
                         </li>
                         <li className="navbar__item">
-                            <a className="navbar__link" href="#" target="_blank"><img class="navbar__social-icon" src={VK}
+                            <a className="navbar__link" href="#" target="_blank"><img className="navbar__social-icon" src={telegram}
                                 alt="Facebook" /></a>
                         </li>
                         <li className="navbar__item">
-                            <a className="navbar__link" href="#" target="_blank"><img class="navbar__social-icon" src={github}
+                            <a className="navbar__link" href="#" target="_blank"><img className="navbar__social-icon" src={github}
                                 alt="Facebook" /></a>
                         </li>
                     </ul>
@@ -46,7 +47,7 @@ function App() {
                         <p className="places__url-heading">URL</p>
                         <a className="places__link" href="http://park-kosa.ru">park-kosa.ru</a>
                     </div> */}
-                    <img className="places__image" src={ProfilePic} alt="Куршская коса" />
+                    <img className="places__image" src={ProfilePic} alt="Фотография профиля" />
                     <div className="places__grid-paragraph">
                         <p className="places__paragraph">I Code and Develop website and web apps. Skilled in HTML5, CSS3, Javascript, React.js, Node.js, Express knowledge. Believe in constant learning that enhances my coding skills more and more!!! </p>
                         {/* <p className="places__paragraph">На этом Калининградская область не заканчивается. Для путешественника
@@ -57,146 +58,73 @@ function App() {
                 </div>
             </section>
 
-            <section id="projects">
-                <div class="container">
-                    <div class="project-wrapper">
-                        <h2 class="section-title dark-blue-text">
-                            Projects
-          </h2>
-
-
-
-                        <div class="row">
-                            <div class="col-lg-4 col-sm-12">
-                                <div class="project-wrapper__text">
-                                    <h3 class="project-wrapper__text-title">Robofriends</h3>
-                                    <div>
-                                        <p class="mb-4">
-                                            A Robofriends website having robots fetched from API along with search option using react.js
-                  </p>
-                                    </div>
-                                    <a target="_blank" class="cta-btn cta-btn--hero" href="https://ritika-soni.github.io/robofriends/">
-                                        See Live
+            <section className="projects">
+                <h2 className="projects__title">Projects</h2>
+                <div className="projects__container">
+                    <div className="projects__description-wrapper">
+                        <h2 className="projects__description-title">How to learn</h2>
+                        <p className="projects__description">A Robofriends website having robots fetched
+                    from API along with search option using react.js</p>
+                        <a target="_blank" className="projects__link" href="https://github.com/Sandhani1704/how-to-learn">
+                            See Live
                 </a>
-                                    <a target="_blank" class="cta-btn text-color-main" href="https://github.com/Ritika-soni/robofriends">
-                                        Source Code
-                </a>
-                                </div>
-                            </div>
-                            <div class="col-lg-8 col-sm-12">
-                                <div class="project-wrapper__image">
-                                    <a href="https://ritika-soni.github.io/robofriends/" target="_blank">
-                                        <div data-tilt class="thumbnail rounded">
-                                            <img class="img-fluid" src="./assets/banner.png" />
-                                        </div>
-                                    </a>
-                                </div>
-                            </div>
-                        </div>
-
-
-
-                        <div class="row">
-                            <div class="col-lg-4 col-sm-12">
-                                <div class="project-wrapper__text">
-                                    <h3 class="project-wrapper__text-title">Covid19 First Case</h3>
-                                    <div>
-                                        <p class="mb-4">
-                                            A website that shows the arrival of first corona virus case in different countries along with date and place using just html and css.
-                  </p>
-                                    </div>
-                                    <a target="_blank" class="cta-btn cta-btn--hero" href="https://ritika-soni.github.io/covid19firstcase/">
-                                        See Live
-                </a>
-                                    <a target="_blank" class="cta-btn text-color-main" href="https://github.com/Ritika-soni/covid19firstcase">
-                                        Source Code
-                </a>
-                                </div>
-                            </div>
-                            <div class="col-lg-8 col-sm-12">
-                                <div class="project-wrapper__image">
-                                    <a href="https://ritika-soni.github.io/covid19firstcase/" target="_blank">
-                                        <div data-tilt class="thumbnail rounded">
-                                            <img class="img-fluid" src="./assets/banner1.png" />
-                                        </div>
-                                    </a>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="row">
-                            <div class="col-lg-4 col-sm-12">
-                                <div class="project-wrapper__text">
-                                    <h3 class="project-wrapper__text-title">Walking Warrior</h3>
-                                    <div>
-                                        <p class="mb-4">
-                                            A codepen animation page with a warrior walking in a dark road using just html and css.
-                  </p>
-                                    </div>
-                                    <a target="_blank" class="cta-btn cta-btn--hero" href="https://codepen.io/ritika_soni/full/JjYLYgM">
-                                        See Live
-                </a>
-                                    <a target="_blank" class="cta-btn text-color-main" href="https://codepen.io/ritika_soni/pen/JjYLYgM">
-                                        Source Code
-                </a>
-                                </div>
-                            </div>
-                            <div class="col-lg-8 col-sm-12">
-                                <div class="project-wrapper__image">
-                                    <a href="https://codepen.io/ritika_soni/full/JjYLYgM" target="_blank">
-                                        <div data-tilt class="thumbnail rounded">
-                                            <img class="img-fluid" src="./assets/banner3.png" />
-                                        </div>
-                                    </a>
-                                </div>
-                            </div>
-                        </div>
-
-
-
-                        <div class="row">
-                            <div class="col-lg-4 col-sm-12">
-                                <div class="project-wrapper__text">
-                                    <h3 class="project-wrapper__text-title">Cakegaadi</h3>
-                                    <div>
-                                        <p class="mb-4">
-                                            A wordpress website for selling cake
-              </p>
-                                    </div>
-                                    <a target="_blank" class="cta-btn cta-btn--hero" href="https://cakegaadishop.wordpress.com/">
-                                        See Live
-            </a>
-                                </div>
-                            </div>
-                            <div class="col-lg-8 col-sm-12">
-                                <div class="project-wrapper__image">
-                                    <a href="https://cakegaadishop.wordpress.com/" target="_blank">
-                                        <div data-tilt class="thumbnail rounded">
-                                            <img class="img-fluid" src="./assets/banner4.png" />
-                                        </div>
-                                    </a>
-                                </div>
-                            </div>
-                        </div>
-
                     </div>
+                    <div className="projects__wrapper-image">
+                        <a target="_blank" className="projects__link" href="https://github.com/Sandhani1704/how-to-learn">
+                            <img className="projects__image" src={ProjectsImage} alt="баннер проекта" />
+                        </a>
+                    </div>
+
                 </div>
+
+                <div className="projects__container">
+                    <div className="projects__description-wrapper">
+                        <h2 className="projects__description-title">How to learn</h2>
+                        <p className="projects__description">A Robofriends website having robots fetched
+                    from API along with search option using react.js</p>
+                        <a target="_blank" className="projects__link" href="https://github.com/Sandhani1704/how-to-learn">
+                            See Live
+                </a>
+                    </div>
+                    <div className="projects__wrapper-image">
+                        <a target="_blank" className="projects__link" href="https://github.com/Sandhani1704/how-to-learn">
+                            <img className="projects__image" src={ProjectsImage} alt="баннер проекта" />
+                        </a>
+                    </div>
+
+                </div>
+
+                <div className="projects__container">
+                    <div className="projects__description-wrapper">
+                        <h2 className="projects__description-title">How to learn</h2>
+                        <p className="projects__description">A Robofriends website having robots fetched
+                    from API along with search option using react.js</p>
+                        <a target="_blank" className="projects__link" href="https://github.com/Sandhani1704/how-to-learn">
+                            See Live
+                </a>
+                    </div>
+                    <div className="projects__wrapper-image">
+                        <a target="_blank" className="projects__link" href="https://github.com/Sandhani1704/how-to-learn">
+                            <img className="projects__image" src={ProjectsImage} alt="баннер проекта" />
+                        </a>
+                    </div>
+
+                </div>
+
             </section>
 
 
-
-
-            <section id="contact">
-                <div class="container">
-                    <h2 class="section-title">
+            <section className="contact">
+                <div className="contact__container">
+                    <h2 className="contact__title">
                         Wanna Contact
         </h2>
-                    <div class="contact-wrapper">
+                    <div className="contact__wrapper">
                         <a
                             target="_blank"
-                            class="cta-btn cta-btn--resume"
-                            href="mailto:ritikasoni73@gmail.com"
-                        >Click Me ! ritikasoni73@gmail.com</a
+                            className="contact__link"
+                            href="mailto:angalkina1704@gmail.com"
+                        >Click Me ! angalkina1704@gmail.com</a
                         >
                     </div>
                 </div>
@@ -204,23 +132,23 @@ function App() {
 
 
 
-            <footer class="footer navbar-static-bottom">
-                <div class="container">
-                    <a href="#top" class="back-to-top">
-                        <i class="fa fa-angle-up fa-2x" aria-hidden="true"></i>
+            <footer className="footer navbar-static-bottom">
+                <div className="container">
+                    <a href="#top" className="back-to-top">
+                        <i className="fa fa-angle-up fa-2x" aria-hidden="true"></i>
                     </a>
-                    <div class="social-links">
+                    <div className="social-links">
                         <a href="https://twitter.com/ritikasonii" target="_blank">
-                            <i class="fa fa-twitter fa-inverse"></i>
+                            <i className="fa fa-twitter fa-inverse"></i>
                         </a>
                         <a href="https://codepen.io/ritika_soni" target="_blank">
-                            <i class="fa fa-codepen fa-inverse"></i>
+                            <i className="fa fa-codepen fa-inverse"></i>
                         </a>
                         <a href="https://www.linkedin.com/in/ritika-sonii/" target="_blank">
-                            <i class="fa fa-linkedin fa-inverse"></i>
+                            <i className="fa fa-linkedin fa-inverse"></i>
                         </a>
                         <a href="https://github.com/Ritika-soni" target="_blank">
-                            <i class="fa fa-github fa-inverse"></i>
+                            <i className="fa fa-github fa-inverse"></i>
                         </a>
                     </div>
 
