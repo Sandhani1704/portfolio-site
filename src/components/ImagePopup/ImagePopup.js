28ece91c98b8c86e8606ec856345fb3d68eb7e31
Anna Galkina <sandhani1704@yandex.ru>
2021-01-27 02:45:52 +0300

    fix: change some styles

--- src/components/ImagePopup/ImagePopup.js
+++ src/components/ImagePopup/ImagePopup.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import closeIcon from '../../images/Close_Icon.svg'
+// import closeIcon from '../../images/Close_Icon.svg'
 
 import './ImagePopup.css';
 
@@ -7,11 +7,12 @@ import './ImagePopup.css';
 function ImagePopup({ isOpen, onClose, card }) {
 
     return (
+        // <img className="popup-image__close-icon" src={closeIcon} alt="закрыть" />
 
         <section className={`popup-image ${isOpen && 'popup-image_opened'}`}>
             <div className="popup-image__overlay"></div>
             <div className="popup-image__cover">
-                <button type="button" onClick={onClose} className="popup-image__close-button"><img className="popup-image__close-icon" src={closeIcon} alt="закрыть" /></button>
+                <button type="button" onClick={onClose} className="popup-image__close-button"></button> 
                 <h4 className="popup-image__title">{card.name}</h4>
 
                 <div className="popup-image__conteiner">
